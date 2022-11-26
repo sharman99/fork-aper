1fa8a6fe78cba430afbbf621b0aa5a60cdf4f3d4
Neha Sharma <neha.sharma@unity3d.com>
2022-11-26 15:52:05 -0800

    add sonata-test

---
+++ examples/sonata-test/client/src/lib.rs
@@ -0,0 +1,37 @@
+use aper_yew::{
+    StateMachineContainerProgram, StateProgramComponent, StateProgramComponentProps,
+    StateProgramViewComponent, StateProgramViewContext,
+};
+pub use counter_common::{Counter, CounterTransition};
+use std::rc::Rc;
+use wasm_bindgen::prelude::*;
+use yew::prelude::{html, Html};
+
+struct CounterView;
+
+impl StateProgramViewComponent for CounterView {
+    type Program = StateMachineContainerProgram<Counter>;
+
+    fn view(state: Rc<Self::Program>, context: StateProgramViewContext<Self::Program>) -> Html {
+        html! {
+            <div>
+                <p>{&format!("Counter: {}", state.0.value())}</p>
+                <button onclick={context.callback.reform(|_| CounterTransition::Add(1))}>
+                    {"+1"}
+                </button>
+                <button onclick={context.callback.reform(|_| CounterTransition::Subtract(1))}>
+                    {"-1"}
+                </button>
+                <button onclick={context.callback.reform(|_| CounterTransition::Reset)}>
+                    {"Reset"}
+                </button>
+            </div>
+        }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn entry() {
+    let props = StateProgramComponentProps::new("ws");
+    yew::start_app_with_props::<StateProgramComponent<CounterView>>(props);
+}
